6e56f73674c45dac9e388b5307f482ba07f547e1
MorenaFischione <fischione.morena@gmail.com>
2021-10-15 20:22:48 +0200

    correzione file index.php e stampa di title author genre e year

--- index.php
+++ index.php
@@ -3,14 +3,17 @@
 
 include_once __DIR__ . "/database.php";
 
-header("Content-Type: application/json");
-
-echo json_encode($dischi);
 
 ?>
 
 
 <?php foreach($dischi as $singoloElemento) { ?>  
-    <h4><?php echo $singoloElemento["title"]; //stampo autore del singolo elemento ?> </h4> 
+    <h3> <?php echo $singoloElemento["title"]; ?> </h3> 
+    <h4> <?php echo $singoloElemento["author"]; ?>  </h4>
+    <h5> <?php echo $singoloElemento["genre"]; ?> </h5>
+    <p> <?php echo $singoloElemento["year"]; ?>  </p>
+    <br>
+    <hr>
 <?php }  ?>
+
 
