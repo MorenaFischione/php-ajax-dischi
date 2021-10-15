a46ecb3f9f4db4f3d499325fe894c850aba31344
MorenaFischione <fischione.morena@gmail.com>
2021-10-15 20:25:41 +0200

    aggiunta di immagine

--- index.php
+++ index.php
@@ -8,10 +8,11 @@ include_once __DIR__ . "/database.php";
 
 
 <?php foreach($dischi as $singoloElemento) { ?>  
-    <h3> <?php echo $singoloElemento["title"]; ?> </h3> 
-    <h4> <?php echo $singoloElemento["author"]; ?>  </h4>
-    <h5> <?php echo $singoloElemento["genre"]; ?> </h5>
-    <p> <?php echo $singoloElemento["year"]; ?>  </p>
+    <img src="<?php echo $singoloElemento["poster"]; ?>" alt="">
+    <h3> Titolo: <?php echo $singoloElemento["title"]; ?> </h3> 
+    <h4> <strong> Autore: </strong> <?php echo $singoloElemento["author"]; ?>  </h4>
+    <h4> <strong> Genere: </strong> <?php echo $singoloElemento["genre"]; ?> </h4>
+    <p> <strong> year: </strong><?php echo $singoloElemento["year"]; ?>  </p>
     <br>
     <hr>
 <?php }  ?>
